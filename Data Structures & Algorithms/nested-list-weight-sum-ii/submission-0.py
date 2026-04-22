de16c645e29b530121774458c910a61e7bacb7b2
Karim Noorani <59754833+karimnoorani@users.noreply.github.com>
2026-04-22 09:50:51 -0500

    Add: nested-list-weight-sum-ii - submission-0

---
+++ Data Structures & Algorithms/nested-list-weight-sum-ii/submission-0.py
@@ -0,0 +1,64 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSumInverse(self, nestedList: List[NestedInteger]) -> int:
+        def getMaxDepth(item, depth):
+            res = depth
+            for x in item:
+                if not x.isInteger():
+                    res = max(res, getMaxDepth(x.getList(), depth+1))
+            return res
+        
+        maxDepth = getMaxDepth(nestedList, 1)
+        def getSum(item, depth):
+            res = 0
+            for x in item:
+                if x.isInteger():
+                    w = maxDepth - depth + 1
+                    res += x.getInteger()*w
+                else:
+                    res += getSum(x.getList(), depth+1)
+            return res
+        
+        return getSum(nestedList, 1)
