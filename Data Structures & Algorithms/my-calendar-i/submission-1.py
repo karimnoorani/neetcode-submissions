e311633b4c389363e83340239b46f152996edeb0
Karim Noorani <59754833+karimnoorani@users.noreply.github.com>
2026-05-14 21:19:05 -0500

    Add: my-calendar-i - submission-1

---
+++ Data Structures & Algorithms/my-calendar-i/submission-1.py
@@ -0,0 +1,25 @@
+class MyCalendar:
+    
+    def __init__(self):
+        self.times = []
+
+    def book(self, startTime: int, endTime: int) -> bool:
+        L, R = 0, len(self.times)-1
+        print([startTime, endTime], self.times)
+        while L <= R:
+            M = (L + R) // 2
+
+            if startTime >= self.times[M][1]:
+                L = M + 1
+            elif endTime <= self.times[M][0]:
+                R = M - 1
+            else:
+                return False
+        
+        self.times.insert(L, [startTime, endTime])
+        return True
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(startTime,endTime)
